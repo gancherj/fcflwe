0bf84eed7f91a049e6a6ec7d37d12df1551472bb
Joshua Gancher <jrg358@cornell.edu>
2017-02-23 20:21:34 -0500

    bv

--- whp.v
+++ whp.v
@@ -13,42 +13,39 @@ Section whps.
 
 
 
-Definition Prob (n : nat) (P : forall n, Bvector n -> bool) := Pr[x <-$ {0,1}^n; ret P _ x].
+Definition Prob {A : nat -> Set}   (D : forall n, Comp (A n)) (P : forall n, A n -> bool) := fun n => Pr[x <-$ D n; ret P _ x].
 
 
-Definition wlp (P : forall n, Bvector n -> bool) := negligible (fun n => Prob n P). 
-Definition whp (P : forall n, Bvector n -> bool) := negligible (fun n => (1 - Prob n P)%rat).
+Definition wlp {A : nat -> Set} (D : forall n, Comp (A n)) (P : forall n, A n -> bool) := negligible (fun n => Prob D P n). 
+Definition whp {A : nat -> Set} (D : forall n, Comp (A n)) (P : forall n, A n -> bool) := negligible (fun n => (1 - Prob D P n)%rat).
 
-Definition wp_impl (P Q : forall n, Bvector n -> bool) := forall (n : nat) (x : Bvector n), P _ x = true -> Q _ x = true.
+Definition wp_impl {A : nat -> Set} (P Q : forall n, A n -> bool) := forall (n : nat) (x : A n), P n x = true -> Q n x = true.
 
-Theorem prob_le : forall P Q n, wp_impl P Q -> Prob n P <= Prob n Q.
+Theorem prob_le : forall A (D : forall n, Comp (A n)) P Q n, wp_impl P Q -> Prob D P n <= Prob D Q n.
 intuition.
 unfold Prob.
 unfold wp_impl in H.
 comp_skip.
 dist_compute.
-pose (H n x e).
 exfalso.
+pose (H n x e).
 intuition.
 
 unfold leRat.
-unfold rat0.
-unfold rat1.
 unfold bleRat.
 simpl.
 auto.
 Qed.
 
-
-
-Theorem prob_opple : forall P Q n, wp_impl P Q -> 1 - Prob n Q <= 1 - Prob n P.
+Theorem prob_opple : forall A (D : forall n, Comp (A n)) P Q n, wp_impl P Q -> 1 - Prob D Q n <= 1 - Prob D P n.
 intuition.
-pose (prob_le P Q n H).
+pose (prob_le A D P Q n H).
 apply ratSubtract_leRat_r.
 intuition.
 Qed.
 
-Theorem whp_impl : forall P Q, whp P -> wp_impl P Q -> whp Q.
+
+Theorem whp_impl : forall A (D : forall n, Comp (A n)) P Q, whp D P -> wp_impl P Q -> whp D Q.
 intuition.
 unfold whp in *.
 
@@ -58,12 +55,12 @@ destruct (H c).
 exists x.
 intuition.
 pose (H1 x0 pf_nz H2).
-pose (prob_opple P Q x0 H0).
+pose (prob_opple A D P Q x0 H0).
 pose (leRat_trans H3 l).
 intuition.
 Qed.
 
-Theorem wlp_impl : forall P Q, wlp P -> wp_impl Q P -> wlp Q.
+Theorem wlp_impl : forall A (D : forall n, Comp (A n)) P Q, wlp D P -> wp_impl Q P -> wlp D Q.
 intuition.
 unfold wlp in *.
 
@@ -73,10 +70,14 @@ destruct (H c).
 exists x.
 intuition.
 pose (H1 x0 pf_nz H2).
-pose (prob_le Q P x0 H0).
+pose (prob_le A D Q P x0 H0).
 pose (leRat_trans H3 l).
 intuition.
 Qed.
 
+Definition Prob_bv := Prob (fun n => x <-$ {0,1}^n; ret x).
+Definition wlp_bv := wlp (fun n => x <-$ {0,1}^n; ret x).
+Definition whp_bv := whp (fun n => x <-$ {0,1}^n; ret x).
 
+End whps.
 
